b02e56254d307be3d26b1984da556d594cebef3b
Ignacio Pinto <ignaciowebdeveloper@gmail.com>
2023-01-31 01:17:08 -0500

    Second version of the calculator project

--- app.js
+++ app.js
@@ -1,22 +1,25 @@
-let display = document.getElementById('display');
-let resultDisplay = document.getElementById('displayResult')
+let down_display = document.getElementById('display');
+let up_display = document.getElementById('displayResult')
 let allButtons = document.querySelectorAll('.button');
+let back_delete = document.querySelector('.back');
 
 /* ARRAY OF ELEMENTS */
 
-const numbersArray = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+const numbersArray = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'];
 const symbolsArray = ['/', 'x', '-', '+'];
 
-/* ARRAY OF TOTALS */
-
-let total = [];
-let partialTotal = [];
-
-/* STATES */
+/* STATE */
 
 const states = {
-    equal: true,
-};
+    symbol: true,
+}
+
+/* TOTALS */
+
+let total = [];
+let finalTotal = [];
+
+/* FUNCTIONS */
 
 function count(numb) {
     if (Number.isInteger(numb)) {
@@ -34,138 +37,87 @@ function roundNum(n) {
     }
 };
 
+function calculateTwoNumbers(n) {
+    let symbol = n[1];
+    let firstN = parseFloat(n[0]);
+    let secondN = parseFloat(n[2]);
 
+    if(symbol == '+'){
+        return (firstN + secondN);
+    } else if(symbol == '-'){
+        return (firstN  - secondN);
+    } else if(symbol == 'x'){
+        return (firstN  * secondN);
+    } else if(symbol == '/'){
+        return (firstN  / secondN);
+    }
+};
+
+function deleteLastDigit(){
+    total.pop()
+    down_display.textContent = total.join('')
+}
 
 function calculator(n) {
 
     let content = n.textContent;
     
-    const endOfArr = (n) => n[n.length - 1];
-    
-    const calculateTwoNum = (n, e) => {
-      
-        if(e == '+'){       
-            return roundNum((n[0] + endOfArr(n)));
-        } else if(e == '-') {
-            return roundNum(n[0] - endOfArr(n));
-        } else if(e == 'x') {
-            return roundNum(n[0] * endOfArr(n));
-        } else if(e == '/') {
-            return roundNum(n[0]  / endOfArr(n));
-        }
-    };
-
-    const allLogs = () => {
-        return (`resultDisplay = ${resultDisplay.textContent}, 
-        display = ${display.textContent}, 
-        total = ${total}
-        partialTotal = ${partialTotal}, 
-        Equal state = ${states['equal']}`)
-    };
-
-    const clear = () => {
+    function clear() {
         total = [];
-        partialTotal = [];
-        display.innerText = '';
-        resultDisplay.innerText = '';
-        states['equal'] = true;
+        finalTotal = [];
+        down_display.innerText = '';
+        up_display.innerText = '';
+        states['symbol'] = true;
+        console.log(total, down_display.textContent, states['symbol'], finalTotal);
     };
     
 
-    if(content === 'CLEAR'){
-
-        clear();
-
-    } else if(numbersArray.includes(content) && display.textContent != '0' || content == '.' && endOfArr(partialTotal) !== '.' && !partialTotal.includes('.') && display.textContent !== ''){
-
-        if(resultDisplay.textContent == '') {
-
-            partialTotal.push(content)
-            display.innerText = partialTotal.join('');
-            console.log(allLogs()); 
-
-        } else if(resultDisplay.textContent !== '' && states['equal'] == true) {
-            
-            partialTotal.push(content);
-            display.innerText = partialTotal.join('');
-            
-            console.log(allLogs()); 
-
-        } else if(resultDisplay.textContent !== '' && states['equal'] == false && total.length == 3 && content !== '.') {
-
-            console.log(allLogs()); 
-            
-           
-
-            total = [];
-            total.push(parseFloat(display.textContent))
-            display.innerText = '';
-            partialTotal.push(content);
-            display.innerText = partialTotal.join('');
-            resultDisplay.innerText = total.join('');
-            states['equal'] = true;
-            console.log(allLogs()); 
-        }
-        
-        
-
-    } else if(symbolsArray.includes(content)) {
-
-        if(resultDisplay.textContent == '' && display.textContent !== '') {
-            partialTotal = [];
-            total.push(parseFloat(display.innerText))
-            total.push(content)
-            resultDisplay.innerText = total.join('')
-            display.textContent = '';
-            console.log(allLogs()); 
-        }else if (resultDisplay.textContent !== '' && states['equal'] == false && total.length == 3) {
-            
-            partialTotal = [];
-            total = [];
-
-            total.push(parseFloat(display.innerText))
-            total.push(content)
-            resultDisplay.innerText = total.join('')
-            display.textContent = '';
-            states['equal'] = true;
-            console.log(allLogs());   
-
-        } else if(resultDisplay.textContent !== '' && states['equal'] == true && display.textContent !== '' && !symbolsArray.includes(endOfArr(total))) {
-
-            partialTotal = [];
-            
-            total.push(content)
-            total.push(parseFloat(display.innerText))
-            
-            resultDisplay.innerText = total.join('')
-            display.textContent = calculateTwoNum(total, total[1]);;
-            console.log(allLogs()); 
-            states['equal'] = false; 
-        }
-        
-            
-            
-
-    } else if(content == '=' && resultDisplay.textContent !== '' && display.textContent !== '' && states['equal'] == true) {
-
-            if (symbolsArray.includes(endOfArr(total))) {
-
-                states['equal'] = false;
-                let lastSymbol = endOfArr(total);
-                total.push(parseFloat(display.innerText));
-                resultDisplay.innerText = total.join('');
-                display.innerText = calculateTwoNum(total, lastSymbol);
-                partialTotal = [];
-                
-                console.log(allLogs()) ;
-            }
+    function showAll(){
+        return`total = ${total}
+down_display = ${down_display.textContent}`;
     }
+
+    if(numbersArray.includes(content)){
+        
+        if(content != '.' && (down_display.textContent !== '0') || content == '.' && total.length > 0 && !total.includes('.')){
+
+            total.push(content)
+            down_display.textContent = total.join('');
+            
+        } 
+
+    } else if(symbolsArray.includes(content) && down_display.textContent !== '' && states['symbol'] == true){
+
+            total = [];
+            finalTotal.push(parseFloat(down_display.textContent))
+            down_display.textContent = '';
+            finalTotal.push(content);
+            up_display.textContent = finalTotal.join(' '); 
+            states['symbol'] = false;
+            
+        
+
+    } else if(content == '=' && states['symbol'] == false && parseFloat(down_display.textContent) > 0 && finalTotal.length >= 2 && finalTotal.length <= 3) {
+        
+        total = [];
+        finalTotal.push(parseFloat(down_display.textContent));
+        down_display.textContent = '';
+        up_display.textContent = `${finalTotal.join(' ')} = ${roundNum(calculateTwoNumbers(finalTotal))}`;
+        down_display.textContent = roundNum(calculateTwoNumbers(finalTotal));
+        console.log(finalTotal)
+        finalTotal = [];
+        states['symbol'] = true;
+
+
+    } else if (content == 'CLEAR'){
+        clear();  
+
+    }
+    
+
 }
 
 
-allButtons.forEach((n) => {
-    n.addEventListener('click', () => {
-       return calculator(n);
-    })
-})
+allButtons.forEach((n) => {n.addEventListener('click', () => calculator(n) )})
 
+back_delete.addEventListener('click', () => { deleteLastDigit() })
